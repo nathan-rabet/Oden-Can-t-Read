77eb3ee5c13df97a0545f49b99219be968165b5e
Maxence Oden <maxenceoden@orange.fr>
2020-12-13 14:33:38 +0100

    Premier test

--- main.c
+++ main.c
@@ -20,14 +20,13 @@ int main()
    mustcall();
 
    char cwd[PATH_MAX];
+   char cwd1[PATH_MAX];
+   char cwd2[PATH_MAX];
    char *datasetpath = strcat(getcwd(cwd, sizeof(cwd)), "/data/dataset/by_class");
-   char *networkpath = strcat(getcwd(cwd, sizeof(cwd)), "/networks.json");
-   char *imagepath = strcat(getcwd(cwd, sizeof(cwd)), "/data/images/rota3.png");
+   char *networkpath = strcat(getcwd(cwd1, sizeof(cwd1)), "/networks.json");
+   char *imagepath = strcat(getcwd(cwd2, sizeof(cwd2)), "/data/images/epita.png");
 
-   size_t npl[] = {NB_INPUTS, 20, 20, 1};
-   char afpl[] = {0, 2, 2, 2};
-   struct Networks *networks = generateRandomNetworks(4, npl, afpl);
-   //struct Networks *networks = LoadNetworksFromJSON(networkpath);
+   struct Networks *networks = LoadNetworksFromJSON(networkpath);
    
    struct Characters *Letters_Matrix = Segmentation(imagepath);
 
--- maintest.c
+++ maintest.c
@@ -30,7 +30,6 @@ int main()
    
 
    FreeNetworks(networks);
-   
 
    
 
