d4466d35b9ed76130eb445182654603a6abe3217
Noé TOPEZA <noe.topeza@epita.fr>
2020-12-15 11:32:31 +0100

    here we go

--- OCR_project.c
+++ OCR_project.c
@@ -10,19 +10,15 @@ GtkWidget *window;
 GtkWidget *binarization;
 GtkWidget *Read_text;
 GtkWidget *Save;
-GtkWidget *OCR;
 GtkWidget *img_input;
-GtkWidget *network_input;
 GtkWidget *save_name;
 GtkWidget *image;
 GtkWidget *text;
-GtkWidget *no_img;
-GtkWidget *no_txt;
 GtkBuilder *builder;
 
-char img_path[];
-char save_folder_path[];
-char txt[];
+char *img_path;
+char *save_folder_path;
+char *txt;
 char save_file_name[128];
 
 // Main function.
@@ -30,7 +26,7 @@ int main () {
         // Initializes GTK.
         gtk_init(NULL, NULL);
 
-	builder = gtk_builder_new_from_file ("OCR_interface.glade");
+	builder = gtk_builder_new_from_file ("OCR_project.glade");
 	
 	window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
 
@@ -41,14 +37,9 @@ int main () {
 	binarization = GTK_WIDGET(gtk_builder_get_object(builder, "binarization"));
 	Read_text = GTK_WIDGET(gtk_builder_get_object(builder, "Read_text"));
 	Save = GTK_WIDGET(gtk_builder_get_object(builder, "Save"));
-	OCR = GTK_WIDGET(gtk_builder_get_object(builder, "OCR"));
 	img_input = GTK_WIDGET(gtk_builder_get_object(builder, "img_input"));
-	network_input = GTK_WIDGET(gtk_builder_get_object(builder, "network_input"));
 	save_name = GTK_WIDGET(gtk_builder_get_object(builder, "save_name"));
-	image = GTK_WIDGET(gtk_builder_get_object(builder, "image"));
 	text = GTK_WIDGET(gtk_builder_get_object(builder, "text"));
-	no_img = GTK_WIDGET(gtk_builder_get_object(builder, "no_img"));
-	no_txt = GTK_WIDGET(gtk_builder_get_object(builder, "no_txt"));
 
 	gtk_widget_show(window);
 
@@ -62,12 +53,10 @@ void on_binarization_clicked (GtkButton *b) {
 	//file = binarization(file)
 	//On ferme précèdente fenetre avec img si non déjà fermé
 	//fc SDL pour print img binarisé et segmenté dans une autre fenetre
-	gtk_label_set_text(GTK_LABEL(no_txt), (const gchar*) "");
 }
 
 void on_Read_text_clicked (GtkButton *b) {
 	//prend le fichier avec image segmenté et input son txt dans la zone
-	txt[50] = "help";
 }	
 
 void on_Save_clicked (GtkButton *b) { //sauvegarde txt 
@@ -79,16 +68,12 @@ void on_Save_clicked (GtkButton *b) { //sauvegarde txt
 void on_img_input_file_set(GtkFileChooserButton *f) {
 	printf("file name = %s\n",gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(f)));
 	//fc SDL pour print img dans une autre fenetre
-	gtk_label_set_text(GTK_LABEL(no_img), (const gchar*) "");
 }
 
-void on_network_input_file_set(GtkFileChooserButton *f) {
-	printf("[filechooser] got the file %s", gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(f)));
-}
 
 void on_save_name_changed(GtkEntry *e) {
 	char save_file_name[128];
-	sprintf(save_file_name, "entry=\s\n", gtk_entry_get_text(e));
+	sprintf(save_file_name, "entry=%s\n", gtk_entry_get_text(e));
 }
 
 // called when window is closed
