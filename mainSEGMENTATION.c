b6dba1ef2b58bf3707ceeea6c48f29ddfc4e57c3
Maxence Oden <maxenceoden@orange.fr>
2020-12-15 23:33:48 +0100

    Cleaning

--- mainSEGMENTATION.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "src/segmentation/segmentation.h"
-
-int main()
-{
-   Segmentation("/home/maxou/Documents/gitrepos/noe.topeza/data/images/rota3.png");
-}
